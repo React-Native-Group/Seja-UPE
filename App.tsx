18faf5647dee4bf1b0a4620527cf0e8006196bc4
Muryllo <muryllopimenta@gmail.com>
2021-11-07 23:23:45 -0300

    :goal_net: feature: Added an Global Exception Handler (Sentry).

--- App.tsx
+++ App.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Sentry from 'sentry-expo';
 
 import { DrawerRoutes } from './src/routes';
 import { GlobalProvider, ThemeProvider } from './src/core/providers';
+
+Sentry.init({
+  dsn: 'https://89fdc0e111744aac87631c0b2adba9fe@o1062890.ingest.sentry.io/6053318',
+  enableInExpoDevelopment: false,
+  debug: false
+});
 
 export default function App() {
   return (
