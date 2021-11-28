4cdd707100f94310fddcfe3a1cc02bf901ca5813
MurylloEx <muryllopimenta@gmail.com>
2021-11-27 23:03:46 -0300

    :sparkles: feature: Added support to Spinner in App pages.

--- src/core/components/PageLayout/index.tsx
+++ src/core/components/PageLayout/index.tsx
@@ -1,12 +1,13 @@
 import React, { Fragment, FunctionComponent, useEffect } from 'react';
+import { ActivityIndicator, BackHandler, Dimensions  } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import Constants from 'expo-constants';
 import Fab from 'react-native-fab';
 
 import { Header } from '../Header';
 import { Render } from '../Render';
 import { useTheme } from '../../hooks';
-import { AreaView, Container, ScrollableContainer } from './styles';
-import { BackHandler } from 'react-native';
+import { AreaView, Center, Container, ScrollableContainer } from './styles';
 
 export interface PageLayoutProps {
   canScroll?: boolean;
@@ -14,6 +15,7 @@ export interface PageLayoutProps {
   showHeader?: boolean;
   showFab?: boolean;
   showTabs?: boolean;
+  showSpinner?: boolean;
   fabIcon?: keyof typeof FontAwesome.glyphMap;
   onFabClick?: () => void;
   onTabClick?: (e: 'suggestions' | 'search') => void;
@@ -24,10 +26,17 @@ export interface PageLayoutProps {
 export const PageLayout: FunctionComponent<PageLayoutProps> = (props) => {
   const [ theme ] = useTheme();
 
-  const { canGoBack, canScroll, showHeader } = props;
-  const { showFab, showTabs, fabIcon } = props;
-  const { onFabClick, onTabClick, children } = props;
-  const { onBackPressed } = props;
+  const { onFabClick, onTabClick, onBackPressed } = props;
+  const { showFab, showTabs, fabIcon, showSpinner } = props;
+  const { showHeader, canGoBack, canScroll, children } = props;
+
+  const { height } = Dimensions.get('window');
+
+  function getSpinnerHeight(){
+    if (!!showHeader)
+      return height - (Constants.statusBarHeight + (!!showTabs ? 102 : 51));
+    return height;
+  }
 
   useEffect(() => {
     function backButtonHandler() {
@@ -39,43 +48,69 @@ export const PageLayout: FunctionComponent<PageLayoutProps> = (props) => {
 
   return (
     <Fragment>
-      <Render if={!!canScroll}>
+
+      <Render if={!!showSpinner}>
         <ScrollableContainer nestedScrollEnabled>
           <Render if={!!showHeader}>
-            <Header 
-              canGoBack={!!canGoBack} 
-              showTabs={showTabs} 
+            <Header
+              canGoBack={!!canGoBack}
+              showTabs={showTabs}
               onTabClick={onTabClick}
               onBackPressed={onBackPressed}
             />
           </Render>
-          <AreaView>
-            {children}
-          </AreaView>
+          <Center style={{ height: getSpinnerHeight() }}>
+            <ActivityIndicator color={theme.red} size={48} />
+          </Center>
         </ScrollableContainer>
       </Render>
-      <Render if={!canScroll}>
-        <Container>
-          <Render if={!!showHeader}>
-            <Header 
-              canGoBack={!!canGoBack} 
-              showTabs={showTabs} 
-              onTabClick={onTabClick}
-              onBackPressed={onBackPressed}
-            />
-          </Render>
-          <AreaView>
-            {children}
-          </AreaView>
-        </Container>
+
+      <Render if={!showSpinner}>
+        <Render if={!!canScroll}>
+          <ScrollableContainer nestedScrollEnabled>
+            <Render if={!!showHeader}>
+              <Header 
+                canGoBack={!!canGoBack} 
+                showTabs={showTabs} 
+                onTabClick={onTabClick}
+                onBackPressed={onBackPressed}
+              />
+            </Render>
+
+            <AreaView>
+              {children}
+            </AreaView>
+          </ScrollableContainer>
+        </Render>
+
+        <Render if={!canScroll}>
+          <Container>
+            <Render if={!!showHeader}>
+              <Header 
+                canGoBack={!!canGoBack} 
+                showTabs={showTabs} 
+                onTabClick={onTabClick}
+                onBackPressed={onBackPressed}
+              />
+            </Render>
+            
+            <AreaView>
+              {children}
+            </AreaView>
+          </Container>
+        </Render>
       </Render>
-      <Fab
-        buttonColor={theme.white}
-        iconTextColor={theme.blue}
-        onClickAction={onFabClick ?? (() => {})}
-        visible={!!showFab}
-        iconTextComponent={<FontAwesome name={fabIcon ?? "comments"} size={24} />}
-      />
+
+      <Render if={!showSpinner}>
+        <Fab
+          buttonColor={theme.white}
+          iconTextColor={theme.blue}
+          onClickAction={onFabClick ?? (() => {})}
+          visible={!!showFab}
+          iconTextComponent={<FontAwesome name={fabIcon ?? "comments"} size={24} />}
+        />
+      </Render>
+
     </Fragment>
   );
 }
--- src/core/components/PageLayout/styles.tsx
+++ src/core/components/PageLayout/styles.tsx
@@ -10,7 +10,12 @@ export const ScrollableContainer = styled.ScrollView`
 
 export const AreaView = styled.View`
   padding-top: 16px;
-  padding-left: 32px;
-  padding-right: 32px;
+  padding-left: 24px;
+  padding-right: 24px;
   padding-bottom: 32px;
+`;
+
+export const Center = styled.View`
+  justify-content: center;
+  align-items: center;
 `;
