072a4f93b9b0e94ada8b8f04ff666c95dc94a376
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-12-01 21:31:09 -0300

    :white_check_mark: tests: added component to CardCourse for test.

---
+++ tests/components/CardCourse.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AssetCardCourseLogo } from '../../src/assets';
+import { CardCourse } from '../../src/core/components';
+import { render } from "../core";
+
+test('render of CardCourse', async () => {
+
+  render(
+    <CardCourse text="Engenharia de Software" banner={AssetCardCourseLogo}/>
+  );
+
+});
