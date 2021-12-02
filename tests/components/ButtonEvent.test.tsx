82a20e11f1707c434c307646b027f91e9665ac5b
brenolyES <brenoly.porto@upe.br>
2021-12-01 22:07:40 -0300

    :white_check_mark: test: buttonEvent.

---
+++ tests/components/ButtonEvent.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ButtonEvent } from '../../src/core/components';
+import { render } from "../core";
+
+test('render of ButtonEvent', async () => {
+
+  render(
+    <ButtonEvent 
+      title="ButtonEvent"
+    />
+  );
+
+});
