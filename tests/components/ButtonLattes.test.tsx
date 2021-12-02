2f5bfa34f6d4ad5bb2c0c18889780fd60254ff38
brenolyES <brenoly.porto@upe.br>
2021-12-01 22:13:10 -0300

    :white_check_mark: test: buttonLattes.

---
+++ tests/components/ButtonLattes.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ButtonLattes } from '../../src/core/components';
+import { render } from "../core";
+
+test('render of ButtonLattes', async () => {
+
+  render(
+    <ButtonLattes 
+      onPress={()=>{}}
+    />
+  );
+
+});
