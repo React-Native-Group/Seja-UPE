75e643021bd6372f9234c48a6489201a1c74a966
Lucas Henrique <lucasmonte2011@gmail.com>
2021-12-01 22:34:59 -0300

    :white_check_mark: tests: adding render test of Render component

---
+++ tests/components/Render.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Paragraph, Render } from "../../src/core/components";
+import { render } from "../core";
+
+test('render of Render', () => {
+  render(
+    <Render if={false}>
+      <Paragraph>
+        Hello!
+      </Paragraph>
+    </Render>
+  )
+});
