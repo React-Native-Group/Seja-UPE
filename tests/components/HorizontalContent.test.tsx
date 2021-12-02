122d050100d5fc8a29acaa31d5de457736fc8120
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-12-01 22:15:33 -0300

    :white_check_mark: tests: added component to HorizontalContent for test.

---
+++ tests/components/HorizontalContent.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HorizontalContent } from '../../src/core/components';
+import { render } from "../core";
+
+test('render of HorizontalContent', async () => {
+
+  render(
+    <HorizontalContent />
+  );
+
+});
