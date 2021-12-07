241270a52342828432971b3035e0bdc35719faad
MurylloEx <muryllopimenta@gmail.com>
2021-12-06 21:37:26 -0300

    :iphone: fix: Layout problem in ButtonCourse component.

--- src/core/components/ButtonCourse/styles.tsx
+++ src/core/components/ButtonCourse/styles.tsx
@@ -15,9 +15,19 @@ export const Container = styled(Ripple).attrs<ThemeValue>(props => props)<ThemeV
   background-color: ${props => props.white};
 `;
 
+export const TitleContainer = styled.View`
+  flex: 1;  
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 export const Title = styled.Text.attrs<ThemeValue>(props => props)<ThemeValue>`
   padding-left: 16px;
-  padding-right: 32px;
+  padding-right: 16px;
+  flex-grow: 1;
   font-size: 18px;  
   font-weight: bold;
   color: ${props => props.blue};
