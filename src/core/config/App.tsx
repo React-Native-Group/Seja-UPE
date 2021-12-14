86fcbd385b04e77f8d3a8fa0c96542eaaad9842a
MurylloEx <muryllopimenta@gmail.com>
2021-12-14 20:37:37 -0300

    :bookmark: release: Bump App version to 3.262.88.

--- src/core/config/App.tsx
+++ src/core/config/App.tsx
@@ -1,3 +1,3 @@
 
-export const AppVersion = "2.249.75";
+export const AppVersion = "3.262.88";
 export const SentryDsn = "https://89fdc0e111744aac87631c0b2adba9fe@o1062890.ingest.sentry.io/6053318";
