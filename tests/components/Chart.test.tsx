8af536686a459ce36566f0f4d111470c9d13e813
Lucas Henrique <lucasmonte2011@gmail.com>
2021-12-21 21:35:49 -0300

    :white_check_mark: tests: implemented layout change test in Chart component.

--- tests/components/Chart.test.tsx
+++ tests/components/Chart.test.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Chart } from '../../src/core/components';
-import { render } from "../core";
+import { fireEvent, render } from "../core";
 
 test('render of Chart', async () => {
 
   render(
     <Chart 
-        legend="Texto de exemplo" 
-        labels={['2017', '2018', '2019', '2020']} 
-        data={[500, 501, 502, 503]} 
-      />
+      legend="Texto de exemplo" 
+      labels={['2017', '2018', '2019', '2020']} 
+      data={[500, 501, 502, 503]} 
+    />
   );
 
 });
+
+test('layout change test', async () => {
+  const { getByTestId } = render(
+    <Chart 
+      legend="Texto de exemplo" 
+      labels={['2017', '2018', '2019', '2020']} 
+      data={[500, 501, 502, 503]} 
+    />
+  );
+  fireEvent(getByTestId('chart.container'), "onLayout", {
+    nativeEvent: {
+      layout: {
+        height: 300, 
+        width: 400, 
+        x: 0, 
+        y: 0
+      }
+    }
+  })
+})
