78226a418a9211456c1e14d5a25e77a59eb18ad3
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-12-21 21:36:57 -0300

    :construction: updated to 63% test of component MultiSlider.

--- tests/components/MultiSlider.test.tsx
+++ tests/components/MultiSlider.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MultiSlider } from '../../src/core/components';
-import { render } from '../core';
+import { fireEvent, render } from "../core";
 
 test('render of MultiSlider', async () => {
   render(
@@ -10,4 +10,24 @@ test('render of MultiSlider', async () => {
       minDistance={10}
     />
   );
+});
+
+test('press MultiSlider', async () => {
+  
+  const { getByTestId } = render(
+    <MultiSlider
+      minValue={1}
+      maxValue={100}
+      minDistance={10}
+      onChange={() => {}}
+    />
+  );
+
+  fireEvent(getByTestId('multislider.container'), 'onChange', { 
+    nativeEvent: { 
+      locationX: 100, 
+      locationY: 100 
+    }
+  });
+
 });
